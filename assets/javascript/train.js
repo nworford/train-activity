02db90e96dec8198775e13fd611946331382b920
nlw113 <nathanworford@gmail.com>
2017-12-12 10:46:29 -0700

    third

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,11 +1,16 @@
 
 
 
-
+console.log("LOADING...");
 $(document).ready(function(){
+	console.log("DONE LOADING!");
 	//firebase.auth()
 	//firebase.database()
 	console.log(firebase);	
+
+
+
+	console.log("PROGRAM COMPLETE");
 });
 
 
