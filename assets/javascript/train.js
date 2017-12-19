3998231b3a591fabed6642d0970ad1eff6b4d369
nlw113 <nathanworford@gmail.com>
2017-12-19 10:49:00 -0700

    v1.03d

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,95 +1,7 @@
 
+console.log("Version 1.03d");
 
-
-console.log("LOADING...");
 $(document).ready(function(){
-
-	console.log("DONE LOADING!");
-	//firebase.auth()
-	//firebase.database()
-	/*
-	console.log(firebase);	
-	var usersRef = firebase.database().ref('users');
-	usersRef.set ({
-		John: {
-			number: 1,
-			age: 30
-		},		
-		Amanda: {
-			number: 2,
-			age: 20
-		}
-	});
-
-	var usersRef2 = firebase.database().ref('users2');
-	usersRef2.set ({
-		John: {
-			number: 1,
-			age: 30
-		},		
-		Amanda: {
-			number: 2,
-			age: 20
-		}
-	});
-
-	var fruitRef = firebase.database().ref('fruits');
-	fruitRef.set ({
-		0: {
-			number: 1,
-			age: 30
-		},		
-		1: {
-			number: 2,
-			age: 20
-		}
-	});
-	*/
-
-	/* 	var trentonRef = firebase.database().ref("trains/Trenton");
-	trentonRef.update ({
-	   "frequency": 92.7
-	});
-	*/
-
-
-/*
-	var trainsRef = firebase.database().ref('trains');
-	trainsRef.set ({
-		Trenton: {
-			number: 1,
-			name: 'Trenton Express',
-			frequency: 25
-		},		
-		Oregon: {
-			number: 2,
-			frequency: 3600
-		}
-	});
-
-//this bulldozes previous pushed data with each user refresh.
-	//when the user pushes submit, make one of these
-	//(we will need trainsRef so that's from way up top)
-	trainsRef.push ({
-		name: "Kukamunga",
-		destination: "(IS that a real place?)",
-		frequency: 11,
-		first: 1234
-	});
-
-	trainsRef.push ({
-		name: "Midnight Carriage2",
-		destination: "Philadelphia1",
-		frequency: 16,
-		first: 1734
-	});
-*/
-	
-/*
-//not working
-var trainsKey = trainsRef.key();
-console.log(trainsKey);
-*/
 	var trainsRef = firebase.database().ref('trains');
 	trainsRef.on('value', function(snap)  {
 		console.log("SOMETHING HAPPENED");
@@ -107,6 +19,26 @@ console.log(trainsKey);
 		});
 
 	});
+});
+
+/*
+//works, but bulldozes past user's data & nothing is permanent
+	//when the user pushes submit, make one of these
+	//(we will need trainsRef so that's from way up top)
+	trainsRef.push ({
+		name: "Kukamunga",
+		destination: "(IS that a real place?)",
+		frequency: 11,
+		first: 1234
+	});
+
+	trainsRef.push ({
+		name: "Midnight Carriage2",
+		destination: "Philadelphia1",
+		frequency: 16,
+		first: 1734
+	});
+*/
 
 /*
 //this requires the random hash be hard coded, thus is not useful
@@ -116,9 +48,71 @@ console.log(trainsKey);
 	});
 */
 
+/*
+//works, but not practical because does not allow creation of new trains.
+	var trainsRef = firebase.database().ref('trains');
+	trainsRef.set ({
+		Trenton: {
+			number: 1,
+			name: 'Trenton Express',
+			frequency: 25
+		},		
+		Oregon: {
+			number: 2,
+			frequency: 3600
+		}
+	});
+*/
 
-	console.log("Version 1.03b");
+/*
+general notes/testing
+console.log(firebase);	
+var usersRef = firebase.database().ref('users');
+usersRef.set ({
+	John: {
+		number: 1,
+		age: 30
+	},		
+	Amanda: {
+		number: 2,
+		age: 20
+	}
 });
 
+var usersRef2 = firebase.database().ref('users2');
+usersRef2.set ({
+	John: {
+		number: 1,
+		age: 30
+	},		
+	Amanda: {
+		number: 2,
+		age: 20
+	}
+});
 
+var fruitRef = firebase.database().ref('fruits');
+fruitRef.set ({
+	0: {
+		number: 1,
+		age: 30
+	},		
+	1: {
+		number: 2,
+		age: 20
+	}
+});
+*/
 
+	
+/*
+//not working
+var trainsKey = trainsRef.key();
+console.log(trainsKey);
+*/
+
+/* 	var trentonRef = firebase.database().ref("trains/Trenton");
+trentonRef.update ({
+   "frequency": 92.7
+});
+*/
