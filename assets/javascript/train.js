f06cecd89b3cc245c625dac17f7b8805d0198291
nlw113 <nathanworford@gmail.com>
2017-12-19 10:38:15 -0700

    v1.03

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -46,6 +46,14 @@ $(document).ready(function(){
 	});
 	*/
 
+	/* 	var trentonRef = firebase.database().ref("trains/Trenton");
+	trentonRef.update ({
+	   "frequency": 92.7
+	});
+	*/
+
+
+/*
 	var trainsRef = firebase.database().ref('trains');
 	trainsRef.set ({
 		Trenton: {
@@ -59,11 +67,7 @@ $(document).ready(function(){
 		}
 	});
 
-	var trentonRef = firebase.database().ref("trains/Trenton");
-	trentonRef.update ({
-	   "frequency": 92.7
-	});
-
+//this bulldozes previous pushed data with each user refresh.
 	//when the user pushes submit, make one of these
 	//(we will need trainsRef so that's from way up top)
 	trainsRef.push ({
@@ -79,19 +83,39 @@ $(document).ready(function(){
 		frequency: 16,
 		first: 1734
 	});
-
+*/
 	
 /*
+*/
+	var trainsRef = firebase.database().ref('trains');
 	var trainsKey = trainsRef.key();
 	console.log(trainsKey);
+	trainsRef.on('value', function(snap)  {
+		console.log("SOMETHING HAPPENED");
+		trainsRef = snap.val();
+	});
 
+	$("#add-train-btn").click(function() {
+
+		trainsRef.push ({
+			name: $("#train-name-input").val();
+			destination: "Philadelphia1",
+			frequency: 16,
+			first: 1734
+		});
+
+	});
+
+/*
+//this requires the random hash be hard coded, thus is not useful
 	var midnightRef = ref.child("trains").child("-L0KyYL0vIntr0Yjq0tW").child('frequency');
 	midnightRef.transaction(function(currentFrq) {
 		return currentFrz + 1;
 	});
 */
 
-	console.log("Version 1.02a");
+
+	console.log("Version 1.03");
 });
 
 
